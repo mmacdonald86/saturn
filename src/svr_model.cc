a6edad11a3dd231e8bf56fc397066ab9130383a4
zepu <Zepu.zhang@groundtruth.com>
2018-08-09 19:50:44 -0700

    support default SVR for -1 traffic

--- src/svr_model.cc
+++ src/svr_model.cc
@@ -5,9 +5,10 @@
 #include "mars/mars.h"
 
 #include <any>
-// #include <cmath> // sqrt
+#include <fstream>
 #include <random>
 #include <tuple>
+
 
 namespace saturn
 {
@@ -50,18 +51,84 @@ SvrModel::SvrModel(FeatureEngine & feature_engine, std::string path)
 
     auto model = mars::CatalogModel::from_avro(areader);
     _mars_model = static_cast<void *>(model.release());
+
+    // Read in default svr file.
+    auto infile_svr = std::ifstream(_path + "/default_svr.txt");
+    if (infile_svr.is_open()) {
+        std::string brand_id;
+        double nonlba_svr, lba_svr;
+        while (infile_svr >> brand_id >> nonlba_svr >> lba_svr) {
+            _default_svr.emplace(brand_id, std::make_tuple(nonlba_svr, lba_svr));
+        }
+        infile_svr.close();
+    } else {
+        throw SaturnError(mars::make_string(
+                              "failed to read file `",
+                              _path + "/default_svr.txt",
+                              "`"));
+    }
 }
+
 
 SvrModel::~SvrModel()
 {
     delete static_cast<mars::CatalogModel *>(_mars_model);
 }
 
+
+double SvrModel::_calc_multiplier(std::string const & brand_id, double user_brand_svr)
+{
+    _feature_engine.update_field(FeatureEngine::FloatField::kUserExtlba, user_brand_svr);
+
+    auto f = static_cast<mars::FeatureEngine *>(_feature_engine._mars_feature_engine);
+    auto x = f->render(_composer_id);
+
+    auto m = static_cast<mars::CatalogModel *>(_mars_model);
+    auto z = m->run(x, brand_id);
+
+    // Version 0:
+    //   CatalogModel contains ChainModel's.
+    //
+    // auto zz = std::any_cast<mars::ChainModel::result_type>(z);
+    // _bid_multiplier = std::any_cast<double>(std::get<0>(zz));
+    // _svr = std::get<1>(zz)[0][0];
+
+    // Version 1:
+    //   CatalogModel contains IsotonicRegression or IsotonicLinearInterpolation.
+    //
+    return std::any_cast<double>(z);
+}
+
+
+double SvrModel::_get_default_svr(std::string const & brand_id, int flag) const
+{
+    try {
+        auto val = _default_svr.at(brand_id);
+        if (flag == 0) {
+            return std::get<0>(val);
+        } else if (flag == 1) {
+            return std::get<1>(val);
+        } else {
+            throw SaturnError(mars::make_string(
+                "unknown flag value `", flag, "`"
+            ));
+        }
+    } catch (std::exception& e) {
+        throw SaturnError(mars::make_string(
+            "failed to get default ",
+            flag == 0 ? "non-LBA" : "LBA",
+            " SVR for brand `",
+            brand_id,
+            "`; additional error message: ",
+            e.what()));
+    }
+}
+
+
 int SvrModel::run(std::string const & brand_id, double user_brand_svr)
 {
     // When `user_brand_svr` is -1, this function provides a brand-aware
     // appropriately small multiplier. 
-    // (To be implemented later; for now it's a dummy value 0.01.)
     // If a campaign does not want to bid on -1 traffic, the logic is in Neptune.
     //
     // In other words, Neptune needs to know whether a specific campaign wants
@@ -69,39 +136,39 @@ int SvrModel::run(std::string const & brand_id, double user_brand_svr)
     // If yes, call this function as usual.
     // If no, do not call this function; just use multiplier 0.
 
+    // For now, LBA default svr and multiplier are not used;
+    // only the non-LBA ones are used.
+
     try {
         _svr = user_brand_svr;
         _message = "";
 
-        // TODO:
-        // provide brand-aware default value for 'user_brand_svr'
-        // if it's not available.
-        // May cache this multiplier per brand, because it can be needed
-        // repeatedly.
         if (user_brand_svr < 0.0) {
-            _bid_multiplier = 0.01;
-            return 0;
+            // Caching default multipliers is because right now
+            // we do not use request-level features.
+            // Once we do use request-level features,
+            // we'll need to re-calculate the multiplier using
+            // the default SVR along with the request-level features.
+
+            auto it = _default_multiplier.find(brand_id);
+            if (it == _default_multiplier.end()) {
+                double nonlba_svr = this->_get_default_svr(brand_id, 0);
+                double nonlba_multiplier = this->_calc_multiplier(brand_id, nonlba_svr);
+                _default_multiplier[brand_id] = std::make_tuple(nonlba_multiplier, -1.0);
+                _bid_multiplier = nonlba_multiplier;                
+            } else {
+                // double lba_multiplier;
+                auto [nonlba_multiplier, lba_multiplier] = std::get<1>(*it);
+                if (nonlba_multiplier < 0.0) {
+                    double nonlba_svr = this->_get_default_svr(brand_id, 0);
+                    nonlba_multiplier = this->_calc_multiplier(brand_id, nonlba_svr);
+                    _default_multiplier[brand_id] = std::make_tuple(nonlba_multiplier, lba_multiplier);
+                }
+                _bid_multiplier = nonlba_multiplier;
+            }
+        } else {
+            _bid_multiplier = this->_calc_multiplier(brand_id, user_brand_svr);
         }
-
-        _feature_engine.update_field(FeatureEngine::FloatField::kUserExtlba, user_brand_svr);
-
-        auto f = static_cast<mars::FeatureEngine *>(_feature_engine._mars_feature_engine);
-        auto x = f->render(_composer_id);
-
-        auto m = static_cast<mars::CatalogModel *>(_mars_model);
-        auto z = m->run(x, brand_id);
-
-        // Version 0:
-        //   CatalogModel contains ChainModel's.
-        //
-        // auto zz = std::any_cast<mars::ChainModel::result_type>(z);
-        // _bid_multiplier = std::any_cast<double>(std::get<0>(zz));
-        // _svr = std::get<1>(zz)[0][0];
-
-        // Version 1:
-        //   CatalogModel contains IsotonicRegression or IsotonicLinearInterpolation.
-        //
-        _bid_multiplier = std::any_cast<double>(z);
 
         return 0;
     } catch (std::exception& e) {
