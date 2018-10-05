3fcd3eef0242b45659414655ea55b29dc03bfdaf
zepu <Zepu.zhang@groundtruth.com>
2018-10-05 10:31:38 -0700

    run_saturn utility script

--- scripts/run_saturn.cc
+++ scripts/run_saturn.cc
@@ -6,35 +6,42 @@
 
 using namespace saturn;
 
+const std::string USAGE = 
+        "Usage:\n"
+        "  run_saturn model_file_path run brand_id adgroup_id psvr\n"
+        "  run_saturn model_file_path has_model adgroup_id";
 
-int main(int argc, char const * const * argv)
+
+int has_model(saturn::SvrModel * svr_model, int argc, char const * const * argv)
 {
-    std::string modelpath;
-    std::string brand_id, adgroup_id;
-    double user_svr;
-
-    if (argc < 5) {
-        std::cout << "Usage:" << std::endl;
-        std::cout << "  run_saturn model_file_path brand_id adgroup_id psvr" << std::endl;
+    if (argc < 4) {
         return 1;
+    }
+    std::string adgroup_id = std::string(argv[3]);
+    auto z = svr_model->has_model(adgroup_id);
+    if (z) {
+        std::cout << "yes" << std::endl;
     } else {
-        modelpath = std::string(argv[1]);
-        while (modelpath.back() == '/') {
-            modelpath.pop_back();
-        }
-        brand_id = std::string(argv[2]);
-        adgroup_id = std::string(argv[3]);
-        user_svr = std::stod(argv[4]);
+        std::cout << "no" << std::endl;
+    }
+    return 0;
+}
+
+
+int run(saturn::SvrModel * svr_model, int argc, char const * const * argv)
+{
+    if (argc < 6) {
+        return 1;
     }
 
-    std::cout << "model file path: " << modelpath << std::endl;
+    std::string brand_id = std::string(argv[3]);
+    std::string adgroup_id = std::string(argv[4]);
+    double user_svr = std::stod(argv[5]);
+
     std::cout << "brand ID:        " << brand_id << std::endl;
     std::cout << "adgroup ID:      " << adgroup_id << std::endl;
     std::cout << "predicted SVR:   " << user_svr << std::endl;
     std::cout << std::endl;
-
-    auto feature_engine = saturn::FeatureEngine();
-    auto svr_model = new saturn::SvrModel(feature_engine, modelpath);
 
     if (svr_model->run(brand_id, adgroup_id, user_svr) == 0) {
         std::cout << "model output" << std::endl;
@@ -45,7 +52,42 @@ int main(int argc, char const * const * argv)
         std::cout << svr_model->message() << std::endl;
     }
 
-    delete svr_model;
-
     return 0;
+}
+
+
+int main(int argc, char const * const * argv)
+{
+    int exit_code = 0;
+
+    if (argc < 3) {
+        exit_code = 1;
+
+    } else {
+        std::string modelpath = std::string(argv[1]);
+        while (modelpath.back() == '/') {
+            modelpath.pop_back();
+        }
+        std::cout << "model file path: " << modelpath << std::endl;
+
+        std::string command = std::string(argv[2]);
+
+        auto feature_engine = saturn::FeatureEngine();
+        auto svr_model = new saturn::SvrModel(feature_engine, modelpath);
+
+        if (command == "run") {
+            exit_code = run(svr_model, argc, argv);
+        } else if (command == "has_model") {
+            exit_code = has_model(svr_model, argc, argv);
+        } else {
+            exit_code = 1;
+        }
+
+        delete svr_model;
+    }
+
+    if (exit_code != 0) {
+        std::cout << USAGE << std::endl;
+    }
+    return exit_code;
 }
