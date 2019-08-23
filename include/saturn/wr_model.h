d5b874e47b1b2ac53faa9c5d55c6ca32ee6dd801
Harikumar Venkatesan <harikumar.venkatesan@groundtruth.com>
2019-08-22 18:33:03 -0700

    --- Rebased to track Develop
    Resolved conflicts manually and changed this branch to track develop.

---
+++ include/saturn/wr_model.h
@@ -0,0 +1,60 @@
+#ifndef _SATURN_WR_MODEL_H_
+#define _SATURN_WR_MODEL_H_
+
+#include "common.h"
+#include "feature_engine.h"
+
+#include <map>
+#include <tuple>
+
+
+namespace saturn
+{
+class WrModel
+{
+  public:
+    WrModel(FeatureEngine & feature_engine, std::string path);
+
+    ~WrModel();
+
+    std::string const & model_id() const;
+
+    double get_prob(std::vector<std::string> input);
+
+    // 0 is success; usually no need to check `message()`.
+    // Other values indicate problems; check `message()`.
+    //
+    // `pacing` is an indicator of how capable the adgroup has been
+    // of spending the allocated budget **in recent times**.
+    // This may not be identical to the 'pacing' concept in Neptune.
+    // This can be a metric based on data of the preceding hour.
+    // For example, in the preceding hour,
+    // the adgroup had an allocated budget of $100 (i.e. had an allowance to spend $100,
+    // unrelated to how the adgroup had been spending before the preceding hour),
+    // and it actually spent $82, then `pacing` is 0.82.
+    //
+    // `pacing` is between 0 and 1, or -1 to use an internal default.
+    //
+    // The default value of `pacing` corresponds to a default curve.
+    // Skip this argument (hence use the default) when the caller is not doing
+    // dynamic multiplier based on pacing.
+
+    // The following methods provide results after a call to `run`.
+    std::string const & message() const;
+    double prob() const;
+
+  private:
+    FeatureEngine & _feature_engine;
+    void * _mars_model = nullptr;
+    double _prob = 0.;
+
+    std::string _path;
+    std::string _model_id;
+    std::string _composer_id;
+
+    std::string _message = "";
+
+};
+
+}  // namespace
+#endif  // include guard
---
+++ scripts/run_ctr.cc
@@ -0,0 +1,69 @@
+#include "saturn/saturn.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "saturn/utils.h"
+
+#include <any>
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace saturn;
+
+const std::string USAGE =
+        "Usage:\n"
+        "  run_Ctr model_file_path run features\n";
+
+
+int run(saturn::ctrModel * ctr_model, int argc, char const * const * argv)
+{
+
+    std::vector<std::string> input;
+     
+    for (int i = 2; i < argc; i++){ 
+        input.push_back(std::string(argv[i]));
+//   std::cout << "input" << argv[i]<< std::endl;
+    }
+  //   for (int i = 0; i< 23; i++){
+  //   std::cout << input[i] << std::endl;
+  //   }
+ //   std::cout << "inside Run"<< std::endl;
+    ctr_model->get_prob(input);
+    return 0;
+}
+
+
+int main(int argc, char const * const * argv)
+{
+    auto timer = Timer();
+    double prob = 0.0;
+    if (argc < 24) { // What is count including features + args. 
+        return 0;
+    } else {
+        std::string modelpath = std::string(argv[1]);
+        while (modelpath.back() == '/') {
+            modelpath.pop_back();
+        }
+        // std::string command = std::string(argv[2]); // Is a run needed? Can we just comment this out?
+
+    auto feature_engine = saturn::FeatureEngine();
+    auto ctr_model = new saturn::ctrModel(feature_engine, modelpath);
+ //   std::cout << "MOdel Parsed from FEATYRE"<< std::endl;
+    timer.start();
+    prob = run(ctr_model, argc, argv);
+    timer.stop();
+    std::cout << "prob:" << ctr_model->prob() << std::endl;
+    delete ctr_model;
+    }
+    std::cout << "Time:" << (double)timer.milliseconds() << "milliseconds"<< std::endl;
+    std::cout << "Probability: " << prob << "prod"<< std::endl;
+    return 1;
+}
